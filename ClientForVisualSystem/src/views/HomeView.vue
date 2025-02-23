5b6e4a41732b20d4225d123e85d06ae6b56e418d
TYApplelive <2623999208@qq.com>
2025-02-23 22:15:11 +0800

    增加了Less插件，reset全局样式，删除了不需要的部分组件

--- ClientForVisualSystem/src/views/HomeView.vue
+++ ClientForVisualSystem/src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="home">
+        <h1>home Component</h1>
+    </div>
 </template>
+
+<script setup lang="ts">
+
+</script>
+<style lang="less" scoped></style>
