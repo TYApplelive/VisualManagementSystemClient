d7b5bae8828c7cb8f934cb59a70c330997757d47
TYApplelive <2623999208@qq.com>
2025-02-27 12:55:17 +0800

    eslint 规范命名

--- src/App.vue
+++ src/App.vue
@@ -11,3 +11,8 @@
   height: 100vh;
 }
 </style>
+
+
+// TODO 剩下任务:
+// TODO 添加可视化数据页面的样式
+// TODO 后端获取NB数据
--- src/views/viusalView/MainView.vue
+++ src/views/viusalView/MainView.vue
@@ -1,57 +1,58 @@
 <template>
-    <div class="visual-main">
-        <div class="common-layout">
-            <el-container>
-                <el-aside>
-                    <menuComponent />
-                </el-aside>
-                <el-main>
-                    <contentComponent />
-                </el-main>
-            </el-container>
-        </div>
+  <div class="visual-main">
+    <div class="common-layout">
+      <el-container>
+        <el-aside>
+          <menuComponent />
+        </el-aside>
+        <el-main>
+          <contentComponent />
+        </el-main>
+      </el-container>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import menuComponent from './c-menu/menu.vue'
+// 菜单栏
+import menuComponent from './c-menu/menuComponent.vue'
 // 内容页面
-import contentComponent from './c-content/content.vue'
+import contentComponent from './c-content/contentView.vue'
 
 </script>
 <style lang="less" scoped>
 .visual-main {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  .common-layout {
     width: 100%;
     height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
 
-    .common-layout {
-        width: 100%;
+    .el-container {
+      width: 100%;
+      height: 100%;
+      display: flex;
+
+      .el-aside {
         height: 100%;
+        background: var(--el-color-primary-light-8);
+        min-width: 200px;
+        flex-shrink: 0;
+      }
 
-        .el-container {
-            width: 100%;
-            height: 100%;
-            display: flex;
-
-            .el-aside {
-                height: 100%;
-                background: var(--el-color-primary-light-8);
-                min-width: 200px;
-                flex-shrink: 0;
-            }
-
-            .el-main {
-                height: 100%;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                flex-grow: 1;
-            }
-        }
+      .el-main {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-grow: 1;
+      }
     }
+  }
 }
 </style>
